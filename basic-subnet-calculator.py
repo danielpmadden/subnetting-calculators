fe40826c9697dd4959c71f03949f5c8b76fa1525
Daniel Madden <44127480+danielpmadden@users.noreply.github.com>
2023-11-14 13:09:31 -0800

    Update basic-subnet-calculator.py

--- basic-subnet-calculator.py
+++ basic-subnet-calculator.py
@@ -1,27 +1,39 @@
 # Function to calculate subnet mask based on the number of subnet bits
-def calculate_subnet_mask(subnet_bits):
-    # Calculate the subnet mask using bitwise operations
-    subnet_mask = (2 ** 32 - 1) ^ (2 ** (32 - subnet_bits) - 1)
-    return subnet_mask
 
-# Main function
-def main():
-    try:
-        # Get user input for the number of subnet bits
-        subnet_bits = int(input("Enter the number of subnet bits: "))
+MAX_SUBNET_BITS = 32 
+
+def get_subnet_bits():
+    # Prompt user for subnet bits and validate input
+    
+    while True:
+        print("Enter the number of subnet bits (0-32):")
+        num_bits = input()
         
-        # Validate the input
-        if subnet_bits < 0 or subnet_bits > 32:
-            print("Invalid subnet bits. Please enter a number between 0 and 32.")
-        else:
-            # Call the calculate_subnet_mask function and display the result
-            subnet_mask = calculate_subnet_mask(subnet_bits)
-            print(f"Subnet mask for {subnet_bits} subnet bits: {subnet_mask}")
-    except ValueError:
-        # Handle invalid input (non-integer input)
-        print("Invalid input. Please enter a valid number.")
+        try:
+            num_bits = int(num_bits)
+            if 0 <= num_bits <= MAX_SUBNET_BITS:
+                return num_bits
+        except ValueError:
+            pass
+        
+        print("Invalid input. Enter an integer between 0 and {MAX_SUBNET_BITS}.")
 
-# Entry point of the program
+def calculate_subnet_mask(num_subnet_bits):
+    # Calculate subnet mask from number of subnet bits
+    
+    return (2**32 - 1) ^ (2**(32 - num_subnet_bits) - 1)
+
+def print_subnet_mask(num_subnet_bits, subnet_mask):
+    # Print subnet mask info to console
+    
+    print("Subnet mask for {num_subnet_bits} subnet bits: {subnet_mask}")
+    
+def main():
+    # Prompt for subnet bits, calculate and display subnet mask
+    
+    num_subnet_bits = get_subnet_bits()
+    subnet_mask = calculate_subnet_mask(num_subnet_bits)
+    print_subnet_mask(num_subnet_bits, subnet_mask)
+
 if __name__ == "__main__":
-    # Call the main function
     main()
